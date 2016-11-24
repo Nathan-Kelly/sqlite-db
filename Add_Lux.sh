dac0bb2116635e2eef0b5b83d060251527faf6f8
Nathan-Kelly <partinarel@gmail.com>
2016-11-24 01:23:35 +0000

    exit statuses now used for signaling java

--- Add_Lux.sh
+++ Add_Lux.sh
@@ -12,35 +12,35 @@ REGEXP="^[+-]?[0-9]+\.?[0-9]*$"
 #arguments: database, name
 if [ -z "$2" ] ; then
     echo "missing argument: dev_id"
-    exit 1
+    exit 2
 fi
 
 if [ -z "$1" ] ; then
     echo "missing argument: db"
-    exit 1
+    exit 2
 fi
 
 #arguments: database, name
 if [ -z "$3" ] ; then
     echo "missing argument: date"
-    exit 1
+    exit 2
 fi
 
 ##check g valid
 if [ -z "$LUX" ] || ! [[ "$LUX" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
     echo "Argument missing or invalid: LUX"
-    exit 1
+    exit 2
 fi
 
 ##check if valid
 
 #->entry       (entry_date, real_date, _dev_id*, [seq])
-if sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO entry VALUES('$EN_DATE', '$RL_DATE', '$DEV_ID', (select max(seq) from entry) + 1); INSERT INTO lux VALUES($LUX,(select max(seq) from entry)); commit;" ; then
+if sqlite3 $DATABASE "PRAGMA foreign_keys = ON; PRAGMA journal_mode = WAL; begin; INSERT INTO entry VALUES('$EN_DATE', '$RL_DATE', '$DEV_ID', (select max(seq) from entry) + 1); INSERT INTO lux VALUES($LUX,(select max(seq) from entry)); commit;" ; then
     echo "s1"
-    exit 0
+    exit
 elif sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO entry VALUES('$EN_DATE', '$RL_DATE', '$DEV_ID', 0); INSERT INTO lux VALUES($LUX, (select max(seq) from entry)); commit;" ; then
     echo "s2"
-    exit 0
+    exit
 fi
 echo "could not enter in database"
-exit 0
+exit 1
