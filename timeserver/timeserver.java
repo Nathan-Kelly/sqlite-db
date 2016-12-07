0ae61c3a7eff726352fce31c969f9bd04bc88a41
Marianne Fletcher <marianneharrison93@gmail.com>
2016-12-08 12:05:08 +1300

    Timeserver accepts an optional argument to set a port number. Removed magic number and replaced with DEFAULT_PORT constant. Added InetAddress to status messages.

--- timeserver/timeserver.java
+++ timeserver/timeserver.java
@@ -2,46 +2,69 @@ import java.io.*;
 import java.net.*;
 
 public class timeserver {
+    
+    static final int DEFAULT_PORT = 65053;
+    static int port;
+    
     public static void main(String[] args) {
+        
+        //use port number if provided
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = DEFAULT_PORT;
+        }
+        
 	try {
-	    ServerSocket ss = new ServerSocket(65053);
+            //open socket
+	    ServerSocket ss = new ServerSocket(port);
 	    
 	    for(;;) {
 		try {
+                    //start new thread on accepting a connection
 		    Socket con = ss.accept();
-		    System.out.println("Connection from " + con.getPort());
+		    System.out.println("Connection from " + con.getInetAddress()
+                            + ":" + con.getPort());
 		    (new Thread(new resThread(con))).start();
-		}
-		catch (Exception e) { //silently ignore exceptions
-		    
-		}
+		} catch (IOException ex) {
+                    System.err.println("Could not handle connection.");
+                }
 	    }
-	}
-	catch (Exception e) {
-	    e.printStackTrace();
-	}
+	} catch (IOException ex) {
+            System.err.println("Could not open TimeServer socket.");
+            ex.printStackTrace();
+        }
     }
 
+    /**
+     * Handles a single connection from a client to the TimeServer.
+     */
     static class resThread implements Runnable {
+        
 	Socket sock;
+        
 	public resThread(Socket sck) {
 	    System.err.println("Thread made");
 	    sock = sck;
 	}
+        
 	public void run() {
 	    try {
 		System.err.println("Thread started");
+                
+                //send current time (in seconds)
 		long cttime = System.currentTimeMillis()/1000L;
 		new DataOutputStream(sock.getOutputStream()).writeBytes(cttime + "\n");
 		System.err.println("Data Sent: " + cttime);
+                
+                //close connection
+                System.err.println("Closing connection to " + sock.getInetAddress()
+                    + ":" + sock.getPort());
 		sock.close();
 		System.err.println("Socket Closed");
-	    }
-	    catch (IOException e) {
-		System.err.println("Error Occured");
-		e.printStackTrace();
-	    }
+	    } catch (IOException ex) {
+                System.err.println("IOException occurred when timesyncing.");
+            }
 	}
     }
 }
-
