f34a25c752b32c2c7a5ae6762cc6b147e2856d1c
Marianne Fletcher <marianneharrison93@gmail.com>
2016-12-09 16:53:19 +1300

    The WindowedSequenceList can report the total number of missing packets since the program was started.

--- WindowedSequenceList.java
+++ WindowedSequenceList.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,7 +17,10 @@ import java.util.logging.Logger;
  * Uses a sliding window scheme to determine when sequence numbers are still
  * valid or can be safely discarded. This allows sequence numbers to overflow.
  *
- * Valid sequence numbers begin at 0 and continue to (distinctSeqNums - 1).
+ * Valid sequence numbers run from 0 to (distinctSeqNums - 1) inclusive.
+ * Missing packets are recorded despite allowing sequence numbers to be removed
+ * and reinserted, however if there is a long gap in sequence numbers
+ * this will be very inaccurate.
  *
  * @author marianne
  */
@@ -25,6 +29,12 @@ public class WindowedSequenceList extends IntervalList {
     protected int distinctSeqNums = 256;
     protected int windowSize = 128;
     protected int windowLHS = 0;    // LHS of sliding window
+    
+    
+    /* The missingCounter keeps track of gaps in sequence numbers once their
+     * section of the number space is going to get discarded.  This happens when
+     * the window is moved. */
+    private int missingCounter = 0;
 
     /**
      * Get the position of the right-hand side of the window. Will wrap around
@@ -69,6 +79,10 @@ public class WindowedSequenceList extends IntervalList {
      */
     @Override
     boolean add(int number) {
+        // boundary checking
+        if (number < 0 || number >= distinctSeqNums) {
+            return false;
+        }
         if (super.add(number)) {
 
             // if not in window, move window
@@ -96,12 +110,23 @@ public class WindowedSequenceList extends IntervalList {
         // check number is in bounds
         if (number < distinctSeqNums && number >= 0) {
             
+            int oldPosition = windowLHS;
+            
             // adjust so RHS aligns with number
             windowLHS += number - getWindowRHS();
             windowLHS %= distinctSeqNums;
             
             if (windowLHS < 0) {
                 windowLHS += distinctSeqNums;
+            }
+            
+            // record any missing packets between sequence numbers now outside
+            // window before they are removed
+            if (windowLHS < oldPosition) {
+                missingCounter += getNumMissingBetween(0, windowLHS);
+                missingCounter += getNumMissingBetween(oldPosition, distinctSeqNums - 1);
+            } else {
+                missingCounter += getNumMissingBetween(oldPosition, windowLHS);
             }
             
             trimOutsideWindow();
@@ -146,7 +171,85 @@ public class WindowedSequenceList extends IntervalList {
     String getWindowCoverage() {
         return String.format("%d-%d", windowLHS, getWindowRHS());
     }
+    
+    /**
+     * Gets the number of packets missing between two sequence numbers (inclusive)
+     * of the numbers themselves if they are not present.  The first argument
+     * must be less than the second argument and the interval does not wrap
+     * around like a window does.
+     * 
+     * This is a complicated function with a lot of different cases.
+     */
+    int getNumMissingBetween(int num1, int num2) {
 
+        int missing = num2 - num1;
+
+        // bounds checking (num1)
+        if (num1 < 0 || num1 >= distinctSeqNums) {
+            return -1;
+        }
+        
+        // bounds checking (num2)
+        if (num2 < 0 || num2 >= distinctSeqNums) {
+            return -1;
+        }
+        
+        if (num2 < num1) {
+            return -1;
+        }
+        
+        Collections.sort(list);
+        
+        if (list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                
+                // interval contains both numbers
+                if (list.get(i).contains(num1) && list.get(i).contains(num2)) {
+                    return 0;
+                    // interval doesn't contain either number
+                } else if (list.get(i).intervalStart > num1 && list.get(i).intervalEnd < num2) {
+                    missing -= list.get(i).intervalEnd - list.get(i).intervalStart + 1;
+                    
+                    // interval contains num1
+                } else if (list.get(i).contains(num1)) {
+                    missing -= list.get(i).intervalEnd - num1 + 1;
+                    
+                    // interval contains num2
+                } else if (list.get(i).contains(num2)) {
+                    missing -= num2 - list.get(i).intervalStart + 1;
+                    
+                    // do not need to check any more Intervals
+                    break;
+                }
+            }
+            
+            return missing;
+        } else {
+            // no sequence numbers received
+            return num2 - num1;
+        }
+    }
+
+    /**
+     * Gets the total number of packets that have been dropped.  This includes
+     * the counter, the number of sequence numbers missing from the start of the
+     * window, and any packets between intervals.
+     * @return 
+     */
+    @Override
+    int getNumMissing() {
+        
+        int missing = 0;
+        
+        if (list.size() > 0) {
+            missing += list.get(0).intervalStart - windowLHS - 1;
+        }
+        
+        missing += super.getNumMissing();
+        missing += missingCounter;
+        return missing;
+    }
+    
     public static void main(String[] args) {
         BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
         WindowedSequenceList list = new WindowedSequenceList();
